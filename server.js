3f9f15f99cd0cd53823881db0767d8ef74fc91f2
Ohm <nuengwong.t@cp.eng.chula.ac.th>
2021-12-14 09:17:02 +0700

    add Authentication

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const cookieParser=require('cookie-parser');
 
 //Load env vars
 dotenv.config({path:'./config/config.env'});
@@ -10,11 +11,17 @@ connectDB();
 
 const app=express();
 
+//add cookie parser
+app.use(cookieParser());
+
 //add body parser
 app.use(express.json());
 
 const hospitals = require('./routes/hospitals');
-app.use('/api/v1/hospitals',hospitals)
+const auth = require('./routes/auth');
+
+app.use('/api/v1/hospitals',hospitals);
+app.use('/api/v1/auth',auth);
 
 const PORT=process.env.PORT || 5000;
 const server=app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
